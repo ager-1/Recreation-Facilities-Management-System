9c1d18edd24f2cff7a21db93e8081ec6e5ada021
Ager <aghazofficial@gmail.com>
2025-08-05 21:57:54 +0800

    Updated

---
+++ Recreational Facilities Management System/Recreational Facilities Management System/Manage_Sports_and_Recreational_Facilities.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Recreational_Facilities_Management_System
+{
+    partial class Manage_Sports_and_Recreational_Facilities
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRepairAndReplace = new System.Windows.Forms.Button();
+            this.btnManageUserAcc = new System.Windows.Forms.Button();
+            this.btnSportsAndRecreational = new System.Windows.Forms.Button();
+            this.btnMaintenance = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.btnRepairAndReplace);
+            this.panel1.Controls.Add(this.btnManageUserAcc);
+            this.panel1.Controls.Add(this.btnSportsAndRecreational);
+            this.panel1.Controls.Add(this.btnMaintenance);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnRepairAndReplace
+            // 
+            this.btnRepairAndReplace.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnRepairAndReplace.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRepairAndReplace.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRepairAndReplace.ForeColor = System.Drawing.Color.White;
+            this.btnRepairAndReplace.Location = new System.Drawing.Point(23, 322);
+            this.btnRepairAndReplace.Name = "btnRepairAndReplace";
+            this.btnRepairAndReplace.Size = new System.Drawing.Size(155, 50);
+            this.btnRepairAndReplace.TabIndex = 7;
+            this.btnRepairAndReplace.Text = "Repair and Replacement";
+            this.btnRepairAndReplace.UseVisualStyleBackColor = false;
+            // 
+            // btnManageUserAcc
+            // 
+            this.btnManageUserAcc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnManageUserAcc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnManageUserAcc.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManageUserAcc.ForeColor = System.Drawing.Color.White;
+            this.btnManageUserAcc.Location = new System.Drawing.Point(23, 74);
+            this.btnManageUserAcc.Name = "btnManageUserAcc";
+            this.btnManageUserAcc.Size = new System.Drawing.Size(155, 50);
+            this.btnManageUserAcc.TabIndex = 4;
+            this.btnManageUserAcc.Text = "User Accounts";
+            this.btnManageUserAcc.UseVisualStyleBackColor = false;
+            // 
+            // btnSportsAndRecreational
+            // 
+            this.btnSportsAndRecreational.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSportsAndRecreational.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSportsAndRecreational.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSportsAndRecreational.ForeColor = System.Drawing.Color.White;
+            this.btnSportsAndRecreational.Location = new System.Drawing.Point(23, 157);
+            this.btnSportsAndRecreational.Name = "btnSportsAndRecreational";
+            this.btnSportsAndRecreational.Size = new System.Drawing.Size(155, 50);
+            this.btnSportsAndRecreational.TabIndex = 5;
+            this.btnSportsAndRecreational.Text = "Sports and Recreational Facilities";
+            this.btnSportsAndRecreational.UseVisualStyleBackColor = false;
+            // 
+            // btnMaintenance
+            // 
+            this.btnMaintenance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnMaintenance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaintenance.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaintenance.ForeColor = System.Drawing.Color.White;
+            this.btnMaintenance.Location = new System.Drawing.Point(23, 242);
+            this.btnMaintenance.Name = "btnMaintenance";
+            this.btnMaintenance.Size = new System.Drawing.Size(155, 50);
+            this.btnMaintenance.TabIndex = 6;
+            this.btnMaintenance.Text = "Maintenance Staff and Schedule";
+            this.btnMaintenance.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(222, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Facility Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(222, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Facility Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(222, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Rate per Hour";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(222, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 28);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Location Info";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(222, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 28);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Availability";
+            // 
+            // Manage_Sports_and_Recreational_Facilities
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Manage_Sports_and_Recreational_Facilities";
+            this.Text = "Manage_Sports_and_Recreational_Facilities";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRepairAndReplace;
+        private System.Windows.Forms.Button btnManageUserAcc;
+        private System.Windows.Forms.Button btnSportsAndRecreational;
+        private System.Windows.Forms.Button btnMaintenance;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
