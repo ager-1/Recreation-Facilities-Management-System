1c18811468e4ebd8e4932875040f1eff007bef17
Ager <aghazofficial@gmail.com>
2025-08-05 21:10:42 +0800

    Updated

---
+++ Recreational Facilities Management System/Recreational Facilities Management System/Manage_User_Accounts.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Recreational_Facilities_Management_System
+{
+    partial class Manage_User_Accounts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRepairAndReplace = new System.Windows.Forms.Button();
+            this.btnMaintenance = new System.Windows.Forms.Button();
+            this.btnSportsAndRecreational = new System.Windows.Forms.Button();
+            this.btnManageUserAcc = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.btnRepairAndReplace);
+            this.panel1.Controls.Add(this.btnManageUserAcc);
+            this.panel1.Controls.Add(this.btnSportsAndRecreational);
+            this.panel1.Controls.Add(this.btnMaintenance);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnRepairAndReplace
+            // 
+            this.btnRepairAndReplace.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnRepairAndReplace.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRepairAndReplace.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRepairAndReplace.ForeColor = System.Drawing.Color.White;
+            this.btnRepairAndReplace.Location = new System.Drawing.Point(23, 322);
+            this.btnRepairAndReplace.Name = "btnRepairAndReplace";
+            this.btnRepairAndReplace.Size = new System.Drawing.Size(155, 50);
+            this.btnRepairAndReplace.TabIndex = 7;
+            this.btnRepairAndReplace.Text = "Repair and Replacement";
+            this.btnRepairAndReplace.UseVisualStyleBackColor = false;
+            // 
+            // btnMaintenance
+            // 
+            this.btnMaintenance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnMaintenance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaintenance.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaintenance.ForeColor = System.Drawing.Color.White;
+            this.btnMaintenance.Location = new System.Drawing.Point(23, 242);
+            this.btnMaintenance.Name = "btnMaintenance";
+            this.btnMaintenance.Size = new System.Drawing.Size(155, 50);
+            this.btnMaintenance.TabIndex = 6;
+            this.btnMaintenance.Text = "Maintenance Staff and Schedule";
+            this.btnMaintenance.UseVisualStyleBackColor = false;
+            // 
+            // btnSportsAndRecreational
+            // 
+            this.btnSportsAndRecreational.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSportsAndRecreational.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSportsAndRecreational.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSportsAndRecreational.ForeColor = System.Drawing.Color.White;
+            this.btnSportsAndRecreational.Location = new System.Drawing.Point(23, 157);
+            this.btnSportsAndRecreational.Name = "btnSportsAndRecreational";
+            this.btnSportsAndRecreational.Size = new System.Drawing.Size(155, 50);
+            this.btnSportsAndRecreational.TabIndex = 5;
+            this.btnSportsAndRecreational.Text = "Sports and Recreational Facilities";
+            this.btnSportsAndRecreational.UseVisualStyleBackColor = false;
+            // 
+            // btnManageUserAcc
+            // 
+            this.btnManageUserAcc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnManageUserAcc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnManageUserAcc.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManageUserAcc.ForeColor = System.Drawing.Color.White;
+            this.btnManageUserAcc.Location = new System.Drawing.Point(23, 74);
+            this.btnManageUserAcc.Name = "btnManageUserAcc";
+            this.btnManageUserAcc.Size = new System.Drawing.Size(155, 50);
+            this.btnManageUserAcc.TabIndex = 4;
+            this.btnManageUserAcc.Text = "User Accounts";
+            this.btnManageUserAcc.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Receptionist",
+            "Maintenance"});
+            this.comboBox1.Location = new System.Drawing.Point(278, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(221, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Role";
+            // 
+            // Manage_User_Accounts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Manage_User_Accounts";
+            this.Text = "Manage_User_Accounts";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRepairAndReplace;
+        private System.Windows.Forms.Button btnManageUserAcc;
+        private System.Windows.Forms.Button btnSportsAndRecreational;
+        private System.Windows.Forms.Button btnMaintenance;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Recreational Facilities Management System/Recreational Facilities Management System/Manage_User_Accounts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recreational_Facilities_Management_System
+{
+    public partial class Manage_User_Accounts : Form
+    {
+        public Manage_User_Accounts()
+        {
+            InitializeComponent();
+        }
+    }
+}
