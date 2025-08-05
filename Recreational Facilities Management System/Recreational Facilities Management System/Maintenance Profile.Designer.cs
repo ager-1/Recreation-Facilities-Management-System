6ce6f78053f9e846390184ffae07ae7652303cd7
OyakiMasu <akzaki17@gmail.com>
2025-08-05 21:22:44 +0800

    added update profile form

---
+++ Recreational Facilities Management System/Recreational Facilities Management System/Maintenance Profile.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Recreational_Facilities_Management_System
+{
+    partial class Maintenance_Profile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtbxName = new System.Windows.Forms.TextBox();
+            this.txtbxNo = new System.Windows.Forms.TextBox();
+            this.txtbxEmail = new System.Windows.Forms.TextBox();
+            this.txtbxPass = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(536, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Update Profile ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(427, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Full Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(427, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contact Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(427, 387);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(427, 480);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password";
+            // 
+            // txtbxName
+            // 
+            this.txtbxName.Location = new System.Drawing.Point(770, 204);
+            this.txtbxName.Name = "txtbxName";
+            this.txtbxName.Size = new System.Drawing.Size(100, 29);
+            this.txtbxName.TabIndex = 5;
+            // 
+            // txtbxNo
+            // 
+            this.txtbxNo.Location = new System.Drawing.Point(770, 290);
+            this.txtbxNo.Name = "txtbxNo";
+            this.txtbxNo.Size = new System.Drawing.Size(100, 29);
+            this.txtbxNo.TabIndex = 6;
+            // 
+            // txtbxEmail
+            // 
+            this.txtbxEmail.Location = new System.Drawing.Point(770, 384);
+            this.txtbxEmail.Name = "txtbxEmail";
+            this.txtbxEmail.Size = new System.Drawing.Size(100, 29);
+            this.txtbxEmail.TabIndex = 7;
+            // 
+            // txtbxPass
+            // 
+            this.txtbxPass.Location = new System.Drawing.Point(779, 489);
+            this.txtbxPass.Name = "txtbxPass";
+            this.txtbxPass.Size = new System.Drawing.Size(100, 29);
+            this.txtbxPass.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Location = new System.Drawing.Point(-7, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(410, 714);
+            this.panel1.TabIndex = 9;
+            // 
+            // Maintenance_Profile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 710);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtbxPass);
+            this.Controls.Add(this.txtbxEmail);
+            this.Controls.Add(this.txtbxNo);
+            this.Controls.Add(this.txtbxName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Maintenance_Profile";
+            this.Text = "Maintenance_Profile";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtbxName;
+        private System.Windows.Forms.TextBox txtbxNo;
+        private System.Windows.Forms.TextBox txtbxEmail;
+        private System.Windows.Forms.TextBox txtbxPass;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
