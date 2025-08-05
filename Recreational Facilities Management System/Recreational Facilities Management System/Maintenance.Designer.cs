adcf1f05b2f63b6fd41136584011b17d959a467f
OyakiMasu <akzaki17@gmail.com>
2025-08-05 20:05:35 +0800

    added maintenance staff dashboard design

--- Recreational Facilities Management System/Recreational Facilities Management System/Maintenance.Designer.cs
+++ Recreational Facilities Management System/Recreational Facilities Management System/Maintenance.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(481, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(661, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Maintenance Staff Dashboard ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 8.142858F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(49, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "View Maintenance Schedule";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Verdana", 8.142858F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(49, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(282, 70);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Update Maintenance Status";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Verdana", 8.142858F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(49, 369);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(282, 68);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Manage Cleaning Supplies";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Verdana", 8.142858F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(49, 482);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(282, 62);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Raise Repair/Replacement Request";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Verdana", 8.142858F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(3, 690);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(188, 49);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Update Profile";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Verdana", 8.142858F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(251, 685);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(182, 54);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Logout";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Location = new System.Drawing.Point(1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(436, 742);
+            this.panel1.TabIndex = 7;
+            // 
+            // Maintenance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1241, 741);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Maintenance";
             this.Text = "Maintenance";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel1;
     }
 }
