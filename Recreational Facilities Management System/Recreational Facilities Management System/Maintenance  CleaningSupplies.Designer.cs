b020e914f1ddeeac272db89e15898e335583fda1
OyakiMasu <akzaki17@gmail.com>
2025-08-05 21:47:54 +0800

    fixing UI issues

--- Recreational Facilities Management System/Recreational Facilities Management System/Maintenance  CleaningSupplies.Designer.cs
+++ Recreational Facilities Management System/Recreational Facilities Management System/Maintenance  CleaningSupplies.Designer.cs
@@ -32,25 +32,33 @@
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnUpdateProfile = new System.Windows.Forms.Button();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.btnViewSchedule = new System.Windows.Forms.Button();
+            this.btnUpdateStatus = new System.Windows.Forms.Button();
+            this.btnMngSupplies = new System.Windows.Forms.Button();
+            this.btnRepair = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
             this.Column3});
-            this.dataGridView1.Location = new System.Drawing.Point(494, 85);
+            this.dataGridView1.Location = new System.Drawing.Point(478, 64);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 72;
             this.dataGridView1.RowTemplate.Height = 31;
-            this.dataGridView1.Size = new System.Drawing.Size(597, 171);
+            this.dataGridView1.Size = new System.Drawing.Size(604, 179);
             this.dataGridView1.TabIndex = 0;
             // 
             // Column1
@@ -74,17 +82,9 @@
             this.Column3.Name = "Column3";
             this.Column3.Width = 175;
             // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Location = new System.Drawing.Point(2, -4);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(415, 798);
-            this.panel1.TabIndex = 1;
-            // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(491, 314);
+            this.btnAdd.Location = new System.Drawing.Point(570, 613);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(132, 43);
             this.btnAdd.TabIndex = 2;
@@ -93,7 +93,7 @@
             // 
             // btnEdit
             // 
-            this.btnEdit.Location = new System.Drawing.Point(655, 314);
+            this.btnEdit.Location = new System.Drawing.Point(778, 613);
             this.btnEdit.Name = "btnEdit";
             this.btnEdit.Size = new System.Drawing.Size(116, 43);
             this.btnEdit.TabIndex = 3;
@@ -102,26 +102,113 @@
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(830, 314);
+            this.btnDelete.Location = new System.Drawing.Point(967, 613);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(115, 43);
             this.btnDelete.TabIndex = 4;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.btnUpdateProfile);
+            this.panel2.Controls.Add(this.btnLogOut);
+            this.panel2.Controls.Add(this.btnViewSchedule);
+            this.panel2.Controls.Add(this.btnUpdateStatus);
+            this.panel2.Controls.Add(this.btnMngSupplies);
+            this.panel2.Controls.Add(this.btnRepair);
+            this.panel2.Location = new System.Drawing.Point(-2, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(425, 675);
+            this.panel2.TabIndex = 5;
+            // 
+            // btnUpdateProfile
+            // 
+            this.btnUpdateProfile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdateProfile.Font = new System.Drawing.Font("Verdana", 8.142858F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateProfile.ForeColor = System.Drawing.Color.White;
+            this.btnUpdateProfile.Location = new System.Drawing.Point(10, 592);
+            this.btnUpdateProfile.Name = "btnUpdateProfile";
+            this.btnUpdateProfile.Size = new System.Drawing.Size(182, 54);
+            this.btnUpdateProfile.TabIndex = 11;
+            this.btnUpdateProfile.Text = "Update Profile";
+            this.btnUpdateProfile.UseVisualStyleBackColor = true;
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogOut.Font = new System.Drawing.Font("Verdana", 8.142858F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogOut.ForeColor = System.Drawing.Color.White;
+            this.btnLogOut.Location = new System.Drawing.Point(209, 592);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(182, 54);
+            this.btnLogOut.TabIndex = 12;
+            this.btnLogOut.Text = "Logout";
+            this.btnLogOut.UseVisualStyleBackColor = true;
+            // 
+            // btnViewSchedule
+            // 
+            this.btnViewSchedule.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewSchedule.Font = new System.Drawing.Font("Verdana", 8.142858F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewSchedule.ForeColor = System.Drawing.Color.White;
+            this.btnViewSchedule.Location = new System.Drawing.Point(55, 83);
+            this.btnViewSchedule.Name = "btnViewSchedule";
+            this.btnViewSchedule.Size = new System.Drawing.Size(282, 68);
+            this.btnViewSchedule.TabIndex = 7;
+            this.btnViewSchedule.Text = "View Maintenance Schedule";
+            this.btnViewSchedule.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdateStatus
+            // 
+            this.btnUpdateStatus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdateStatus.Font = new System.Drawing.Font("Verdana", 8.142858F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateStatus.ForeColor = System.Drawing.Color.White;
+            this.btnUpdateStatus.Location = new System.Drawing.Point(55, 190);
+            this.btnUpdateStatus.Name = "btnUpdateStatus";
+            this.btnUpdateStatus.Size = new System.Drawing.Size(282, 70);
+            this.btnUpdateStatus.TabIndex = 8;
+            this.btnUpdateStatus.Text = "Update Maintenance Status";
+            this.btnUpdateStatus.UseVisualStyleBackColor = true;
+            // 
+            // btnMngSupplies
+            // 
+            this.btnMngSupplies.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMngSupplies.Font = new System.Drawing.Font("Verdana", 8.142858F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMngSupplies.ForeColor = System.Drawing.Color.White;
+            this.btnMngSupplies.Location = new System.Drawing.Point(55, 300);
+            this.btnMngSupplies.Name = "btnMngSupplies";
+            this.btnMngSupplies.Size = new System.Drawing.Size(282, 68);
+            this.btnMngSupplies.TabIndex = 9;
+            this.btnMngSupplies.Text = "Manage Cleaning Supplies";
+            this.btnMngSupplies.UseVisualStyleBackColor = true;
+            // 
+            // btnRepair
+            // 
+            this.btnRepair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRepair.Font = new System.Drawing.Font("Verdana", 8.142858F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRepair.ForeColor = System.Drawing.Color.White;
+            this.btnRepair.Location = new System.Drawing.Point(55, 413);
+            this.btnRepair.Name = "btnRepair";
+            this.btnRepair.Size = new System.Drawing.Size(282, 62);
+            this.btnRepair.TabIndex = 10;
+            this.btnRepair.Text = "Raise Repair/Replacement Request";
+            this.btnRepair.UseVisualStyleBackColor = true;
+            // 
             // Maintenance__CleaningSupplies
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1196, 792);
+            this.ClientSize = new System.Drawing.Size(1195, 677);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Maintenance__CleaningSupplies";
             this.Text = "Maintenance__CleaningSupplies";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -132,9 +219,15 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnUpdateProfile;
+        private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.Button btnViewSchedule;
+        private System.Windows.Forms.Button btnUpdateStatus;
+        private System.Windows.Forms.Button btnMngSupplies;
+        private System.Windows.Forms.Button btnRepair;
     }
 }
