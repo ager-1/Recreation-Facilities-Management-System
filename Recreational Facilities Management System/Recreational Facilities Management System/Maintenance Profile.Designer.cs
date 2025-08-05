92acdadecf2377f0d0151ed228962e263ad83f7d
OyakiMasu <akzaki17@gmail.com>
2025-08-05 21:23:46 +0800

    Added all required forms

--- Recreational Facilities Management System/Recreational Facilities Management System/Maintenance Profile.Designer.cs
+++ Recreational Facilities Management System/Recreational Facilities Management System/Maintenance Profile.Designer.cs
@@ -38,6 +38,7 @@
             this.txtbxEmail = new System.Windows.Forms.TextBox();
             this.txtbxPass = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -122,11 +123,21 @@
             this.panel1.Size = new System.Drawing.Size(410, 714);
             this.panel1.TabIndex = 9;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(604, 595);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 47);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Maintenance_Profile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1090, 710);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.txtbxPass);
             this.Controls.Add(this.txtbxEmail);
@@ -156,5 +167,6 @@
         private System.Windows.Forms.TextBox txtbxEmail;
         private System.Windows.Forms.TextBox txtbxPass;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
